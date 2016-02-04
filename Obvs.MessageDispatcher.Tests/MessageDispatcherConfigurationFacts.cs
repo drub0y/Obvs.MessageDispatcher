aa45613c4708ef9b00b931b0e4b4570057f7b9d6
Drew Marsh <drub0y@hotmail.com>
2016-02-04 14:48:58 -0800

    Started fleshing out configuration extensions.

---
+++ Obvs.MessageDispatcher.Tests/MessageDispatcherConfigurationFacts.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using FluentAssertions;
+using Moq;
+using Obvs.MessageDispatcher.Configuration;
+using Obvs.Types;
+using Xunit;
+
+namespace Obvs.MessageDispatcher.Tests
+{
+    public class MessageDispatcherConfigurationFacts
+    {
+        public class ConstructorFacts
+        {
+            [Fact]
+            public void ThrowsOnNullMessagesObservable()
+            {
+                Action action = () => new MessageDispatcherConfiguration<IMessage>(null);
+
+                action.ShouldThrow<ArgumentNullException>()
+                    .And.ParamName.Should().Be("messages");
+            }
+        }
+
+        public class WithMessageHandlerSelectorFactoryFacts
+        {
+            [Fact]
+            public void ThrowsOnNullFactory()
+            {
+                var serviceBusMock = new Mock<IServiceBus>();
+                serviceBusMock.SetupGet(sb => sb.Commands)
+                    .Returns(Mock.Of<IObservable<ICommand>>());
+
+                var messageDispatcherConfiguration = serviceBusMock.Object.DispatcherFor(sb => sb.Commands);
+
+                Action action = () => messageDispatcherConfiguration.WithMessageHandlerSelectorFactory(null);
+
+                action.ShouldThrow<ArgumentNullException>()
+                    .And.ParamName.Should().Be("messageHandlerSelectorFactory");
+            }
+
+            [Fact]
+            public void ReturnsMessageDispatcherConfiguration()
+            {
+                var serviceBusMock = new Mock<IServiceBus>();
+                serviceBusMock.SetupGet(sb => sb.Commands)
+                    .Returns(Mock.Of<IObservable<ICommand>>());
+
+                var messageDispatcherConfiguration = serviceBusMock.Object.DispatcherFor(sb => sb.Commands);
+
+                var updatedConfiguration = messageDispatcherConfiguration.WithMessageHandlerSelectorFactory(() => Mock.Of<IMessageHandlerSelector>());
+
+                updatedConfiguration.Should().NotBeNull();
+            }
+
+            [Fact]
+            public void MessageDispatcherConfigurationMessageHandlerShouldBeTheConfiguredFactory()
+            {
+                var serviceBusMock = new Mock<IServiceBus>();
+                serviceBusMock.SetupGet(sb => sb.Commands)
+                    .Returns(Mock.Of<IObservable<ICommand>>());
+
+                var messageDispatcherConfiguration = serviceBusMock.Object.DispatcherFor(sb => sb.Commands);
+
+                Func<IMessageHandlerSelector> messageHandlerSelectorFactory = () => Mock.Of<IMessageHandlerSelector>();
+
+                var updatedConfiguration = messageDispatcherConfiguration.WithMessageHandlerSelectorFactory(messageHandlerSelectorFactory);
+
+                ((MessageDispatcherConfiguration<ICommand>)updatedConfiguration).MessageHandlerSelectorFactory.Should().Be(messageHandlerSelectorFactory);
+            }
+        }
+
+        public class RunDispatcherFacts
+        {
+            [Fact]
+            public void ReturnsNonNullObservable()
+            {
+                var serviceBusMock = new Mock<IServiceBus>();
+                serviceBusMock.SetupGet(sb => sb.Commands)
+                    .Returns(Mock.Of<IObservable<ICommand>>());
+
+                var messageDispatcherConfiguration = serviceBusMock.Object.DispatcherFor(sb => sb.Commands);
+
+                var messageDispatchResults = messageDispatcherConfiguration.WithMessageHandlerSelectorFactory(() => Mock.Of<IMessageHandlerSelector>())
+                    .RunDispatcher();
+
+                messageDispatchResults.Should().NotBeNull();
+            }
+        }
+    }
+
+    public class DispatcherForFacts
+    {
+        [Fact]
+        public void ThrowsOnNullServiceBus()
+        {
+            IServiceBus serviceBus = null;
+
+            Action action = () => serviceBus.DispatcherFor(sb => sb.Commands);
+
+            action.ShouldThrow<ArgumentNullException>()
+                .And.ParamName.Should().Be("serviceBus");
+        }
+
+        [Fact]
+        public void ReturnsMessageDispatcherConfiguration()
+        {
+            var serviceBusMock = new Mock<IServiceBus>();
+            serviceBusMock.SetupGet(sb => sb.Commands)
+                .Returns(Mock.Of<IObservable<ICommand>>());
+
+            var messageDispatcherConfiguration = serviceBusMock.Object.DispatcherFor(sb => sb.Commands);
+
+            messageDispatcherConfiguration.Should().NotBeNull();
+        }
+
+        [Fact]
+        public void MessageDispatcherConfigurationMessagesShouldBeTheSelectedMessages()
+        {
+            var commandsObservableMock = Mock.Of<IObservable<ICommand>>();
+
+            var serviceBusMock = new Mock<IServiceBus>();
+            serviceBusMock.SetupGet(sb => sb.Commands)
+                .Returns(commandsObservableMock);
+
+            var messageDispatcherConfiguration = serviceBusMock.Object.DispatcherFor(sb => sb.Commands);
+
+            ((MessageDispatcherConfiguration<ICommand>)messageDispatcherConfiguration).Messages.Should().Be(commandsObservableMock);
+        }
+    }
+}
---
+++ Obvs.MessageDispatcher/Configuration.cs
@@ -0,0 +1,105 @@
+﻿using System;
+
+namespace Obvs.MessageDispatcher.Configuration
+{
+    public interface IMessageDispatcherConfigurationWithFactory<TMessage> : IMessageDispatcherConfiguration<TMessage>
+        where TMessage : class
+    {
+        IObservable<MessageDispatchResult<TMessage>> RunDispatcher();
+    }
+
+    public interface IMessageDispatcherConfiguration<TMessage>
+        where TMessage : class
+    {
+        IMessageDispatcherConfigurationWithFactory<TMessage> WithMessageHandlerSelectorFactory(Func<IMessageHandlerSelector> messageHandlerSelectorFactory);
+    }
+
+    internal class MessageDispatcherConfiguration<TMessage> : IMessageDispatcherConfigurationWithFactory<TMessage>
+        where TMessage : class
+    {
+        private readonly IObservable<TMessage> _messages;
+        private Func<IMessageHandlerSelector> _messageHandlerSelectorFactory;
+
+        public MessageDispatcherConfiguration(IObservable<TMessage> messages)
+        {
+            if(messages == null) throw new ArgumentNullException(nameof(messages));
+
+            _messages = messages;
+        }
+
+        public IObservable<TMessage> Messages => _messages;
+        public Func<IMessageHandlerSelector> MessageHandlerSelectorFactory => _messageHandlerSelectorFactory;
+
+        public IMessageDispatcherConfigurationWithFactory<TMessage> WithMessageHandlerSelectorFactory(Func<IMessageHandlerSelector> messageHandlerSelectorFactory)
+        {
+            if(messageHandlerSelectorFactory == null) throw new ArgumentNullException(nameof(messageHandlerSelectorFactory));
+
+            _messageHandlerSelectorFactory = messageHandlerSelectorFactory;
+
+            return this;
+        }
+
+        public IObservable<MessageDispatchResult<TMessage>> RunDispatcher()
+        {
+            return new MessageDispatcher<TMessage>(_messageHandlerSelectorFactory).Run(_messages);
+        }
+    }
+
+
+    public static class ServiceBusExtensions
+    {
+        public static IMessageDispatcherConfiguration<TTargetMessage> DispatcherFor<TMessage, TCommand, TEvent, TRequest, TResponse, TTargetMessage>(this IServiceBus<TMessage, TCommand, TEvent, TRequest, TResponse> serviceBus, Func<IServiceBus<TMessage, TCommand, TEvent, TRequest, TResponse>, IObservable<TTargetMessage>> messageSelector)
+            where TMessage : class
+            where TCommand : class, TMessage
+            where TEvent : class, TMessage
+            where TRequest : class, TMessage
+            where TResponse : class, TMessage
+            where TTargetMessage : class, TMessage
+        {
+            if(serviceBus == null) throw new ArgumentNullException(nameof(serviceBus));
+
+            return new MessageDispatcherConfiguration<TTargetMessage>(messageSelector(serviceBus));
+        }
+
+        public static IMessageDispatcherConfiguration<TTargetMessage> DispatcherFor<TMessage, TCommand, TEvent, TRequest, TResponse, TTargetMessage>(this IServiceBusClient<TMessage, TCommand, TEvent, TRequest, TResponse> serviceBusClient, Func<IServiceBusClient<TMessage, TCommand, TEvent, TRequest, TResponse>, IObservable<TTargetMessage>> messageSelector)
+            where TMessage : class
+            where TCommand : class, TMessage
+            where TEvent : class, TMessage
+            where TRequest : class, TMessage
+            where TResponse : class, TMessage
+            where TTargetMessage : class, TMessage
+        {
+            if(serviceBusClient == null) throw new ArgumentNullException(nameof(serviceBusClient));
+
+            return new MessageDispatcherConfiguration<TTargetMessage>(messageSelector(serviceBusClient));
+        }
+    }
+
+    public static class ServiceEndpointExtensions
+    {
+        public static IMessageDispatcherConfiguration<TCommand> DispatcherForCommands<TMessage, TCommand, TEvent, TRequest, TResponse>(this IServiceEndpoint<TMessage, TCommand, TEvent, TRequest, TResponse> serviceEndpoint)
+            where TMessage : class
+            where TCommand : class, TMessage
+            where TEvent : class, TMessage
+            where TRequest : class, TMessage
+            where TResponse : class, TMessage
+        {
+            if(serviceEndpoint == null) throw new ArgumentNullException(nameof(serviceEndpoint));
+
+            return new MessageDispatcherConfiguration<TCommand>(serviceEndpoint.Commands);
+        }
+
+        public static IMessageDispatcherConfiguration<TEvent> DispatcherForEvents<TMessage, TCommand, TEvent, TRequest, TResponse>(this IServiceEndpointClient<TMessage, TCommand, TEvent, TRequest, TResponse> serviceEndpointClient)
+            where TMessage : class
+            where TCommand : class, TMessage
+            where TEvent : class, TMessage
+            where TRequest : class, TMessage
+            where TResponse : class, TMessage
+        {
+            if(serviceEndpointClient == null) throw new ArgumentNullException(nameof(serviceEndpointClient));
+
+            return new MessageDispatcherConfiguration<TEvent>(serviceEndpointClient.Events);
+        }
+
+    }
+}
