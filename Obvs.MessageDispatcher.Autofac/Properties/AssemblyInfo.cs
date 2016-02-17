c7556c6cd6bac8b24af0bc00ff5579db90233a83
Drew Marsh <drub0y@hotmail.com>
2016-02-17 11:33:16 -0800

    Adding message handler registration extension for Autofac.

--- Obvs.MessageDispatcher.Autofac/Properties/AssemblyInfo.cs
+++ Obvs.MessageDispatcher.Autofac/Properties/AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyProduct("Obvs.MessageDispatcher")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 
-[assembly: AssemblyVersion("0.1.*")]
-[assembly: AssemblyInformationalVersion("0.1.0-alpha")]
+[assembly: AssemblyVersion("0.2.*")]
+[assembly: AssemblyInformationalVersion("0.2.0-alpha")]
 
 [assembly: InternalsVisibleTo("Obvs.MessageDispatcher.Autofac.Tests")]
